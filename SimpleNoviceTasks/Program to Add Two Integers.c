66b467663f2962a065a36df07124cdc7d91c2011
Kukarazi <46269087+Kukarazi@users.noreply.github.com>
2021-01-10 01:11:39 +0300

    Create Program to Add Two Integers.c

---
+++ SimpleNoviceTasks/Program to Add Two Integers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+  int j,k,sum;
+  
+  printf("Enter first integer: ");
+  scanf("%d", &j);
+  
+  printf("Enter second integer: ");
+  scanf("%d", &k);
+  
+  sum = j+k;
+  printf("The addition result is : %d+%d=%d\n", j+k=sum);
+}
