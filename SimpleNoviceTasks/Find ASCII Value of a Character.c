656603cdce5c2f4ca13b0b1c8265faa0581384ff
Kukarazi <46269087+Kukarazi@users.noreply.github.com>
2021-01-10 19:49:48 +0300

    Create Find ASCII Value of a Character.c

---
+++ SimpleNoviceTasks/Find ASCII Value of a Character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+  char b;
+
+  printf("Please,enter a single character:\n");
+
+  scanf("%c", &b);
+
+  printf("You entered ''%c'' character and it's ASCII value is %d\n", b , b);
+}
