cef87adc2c00c03e50e25e7ad3517e91dd771f60
Kukarazi <46269087+Kukarazi@users.noreply.github.com>
2021-01-13 14:56:55 +0300

    Update half1.c
    Inverted half pyramid of *

--- Control Flow Examples/Pyramids/half1.c
+++ Control Flow Examples/Pyramids/half1.c
@@ -4,7 +4,7 @@ int main(void){
   printf("Enter number of rows\n");
   scanf("%d", &n);
 
-  for(i=n; i>=1; --i){  // from 5 '*' to -1 each row
+  for(i=n; i>=1; --i){  // from 5 '*' to -1 each row  
      for(j=1; j<=i; ++j){
         printf("*");
      }
