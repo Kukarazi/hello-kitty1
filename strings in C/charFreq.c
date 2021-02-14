0fb53e227f1928407e8a7a0ea9e24c028c832b20
Kukarazi <46269087+Kukarazi@users.noreply.github.com>
2021-02-14 17:59:05 +0300

    Create charFreq.c

---
+++ strings in C/charFreq.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int counter = 0;
+    char sent[500];
+    printf("Enter some text: \n");
+    fgets(sent, sizeof(sent), stdin); // Getting string from the user
+
+    char ch;
+    printf("Enter char whose frequency you want to find:\n");
+    scanf("%c", &ch); // getting a char,whose frequency user want to find
+
+    for (int i = 0; i < *sent; i++) // iterating over forLoop,finding char and adding to counter
+    {
+        if (sent[i] == ch)
+        {
+            counter = counter + 1; // counter of frequency of 'ch'
+        }
+        else
+        {
+            continue;
+        }
+    }
+    printf("Counter is: %d\n", counter);
+}
